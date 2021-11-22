76be9d0a37acbc2dd35f1d3e8682989dcc06c3a0
killerdns <divyank.shah@studets.iiit.ac.in>
2021-11-22 11:58:50 +0530

    Made minor changes

--- finalDataset/meregeData.py
+++ finalDataset/meregeData.py
@@ -7,6 +7,12 @@ for file in blogFiles:
     with open(file) as f:
         jsonObj = json.load(f)
         for idx in jsonObj:
+            if('transcript' not in jsonObj[idx]):
+
+                print("FileName",file,idx)
+                break
+            jsonObj[idx]['summary']=jsonObj[idx]['transcript']
+            jsonObj[idx].pop('transcript')
             finalData[str(idr)]=jsonObj[idx]
             idr+=1
 
@@ -14,6 +20,3 @@ for file in blogFiles:
 jsonObj=json.dumps(finalData,indent=4)
 with open('finalData.json','w') as f:
     f.write(jsonObj)
-
-# jsonObj=json.dump(finalData)
-# with open()
